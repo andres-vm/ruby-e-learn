c4868881cd851f0829f980f725cfa34ee29b1a54
andres-vm <andres.vm.mv@gmail.com>
2021-10-04 13:23:55 +0000

    fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,6 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-//import 'bootstrap/dist/js/bootstrap'
-//import 'bootstrap/dist/css/bootstrap'
-//require("../stylesheets/application.scss")
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("../stylesheets/application.scss")
