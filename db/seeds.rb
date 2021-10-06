cf1403026ba4b423df2450aaf8974a10404878a6
andres-vm <andres.vm.mv@gmail.com>
2021-10-06 16:04:18 +0000

    Navbar functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#User.create!(email: 'andres2@andres.es', password: 'andres', password_confirmation: 'andres')
+30.times do
+    Course.create!([{
+        title: Faker::Educator.course_name,
+        description: Faker::TvShows::GameOfThrones.quote,
+        user_id: User.first.id
+    }])
+end
