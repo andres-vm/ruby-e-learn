c89868956711fe8bc3802177fc62b6054bd7fbed
andres-vm <andres.vm.mv@gmail.com>
2021-10-08 16:18:06 +0000

    Fixed navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+    
     def index
-    #@users = User.all.order(created_at: :desc)
-
-    @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+        #if params[:title]
+        #  @courses = Course.where('title ILIKE ?', "%#{params[:title]}%") #case-insensitive
+        #else
+        #  #@courses = Course.all
+        #  
+        #  #@q = Course.ransack(params[:q])
+        #  #@courses = @q.result.includes(:user)
+        #end
+        @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+        @courses = @ransack_courses.result.includes(:user)
     end
+    
 end
