2294c476f1e24493d1f6685833e079ae0653cc29
andres-vm <andres.vm.mv@gmail.com>
2021-10-09 20:49:52 +0000

    Added slug to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,9 @@ class User < ApplicationRecord
   has_many :courses
 
   after_create :assign_default_role
-
+  extend FriendlyId
+  friendly_id :email, use: :slugged
+  
   def assign_default_role
     if User.count == 1
       self.add_role(:admin) if self.roles.blank?
