34583545810c2e2c454aaa1af5c35940a0c5f47a
andres-vm <andres.vm.mv@gmail.com>
2021-10-21 22:21:54 +0000

    Added landing page functionalities

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,12 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:landing_page, :privacy_policy]
   
   def landing_page
-    puts "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
-    puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
     @courses = Course.all.limit(3)
     @latest_couses = Course.all.limit(3).order(created_at: :desc)
+    @latest_good_reviews = Enrollment.reviewed.order(rating: :desc, created_at: :desc).limit(3)
+    @top_rated_courses = Course.order(average_rating: :desc, created_at: :desc).limit(3)
+    @popular_courses = Course.order(enrollments_count: :desc, created_at: :desc).limit(3)
+    @purchased_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).order(created_at: :desc).limit(3)
   end
 
   def privacy_policy
